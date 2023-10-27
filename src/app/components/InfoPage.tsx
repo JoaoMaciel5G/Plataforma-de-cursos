b4932671a3ec13171b8aa7e94b35912aa257b366
JoaoMaciel5G <juauvitu765@gmail.com>
2023-10-27 12:10:39 -0300

    chore: Add components and styles

---
+++ src/app/components/InfoPage.tsx
@@ -0,0 +1,13 @@
+function InfoPage() {
+    return ( 
+        <div className="text-center text-aqua font-semibold mt-20">
+            <h2>Desbloqueie seu maior potencial de estudo, e esteja apto pro mercado de trabalho</h2>
+            <div>
+                <h1 className=""><span>Cursos Online aqui na Pro Tech</span> para turbinar seu aprendizado</h1>
+            </div>
+            
+        </div>
+     );
+}
+
+export default InfoPage;
