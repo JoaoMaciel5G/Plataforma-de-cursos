3a9999b2d78d031baec8ccd576e8f1702bbbf62d
JoaoMaciel5G <juauvitu765@gmail.com>
2024-01-23 19:55:57 -0300

    [fix] fixing problem of login

--- src/app/contexts/login.tsx
+++ src/app/contexts/login.tsx
@@ -7,7 +7,7 @@ export async function loginRequest({email, password}: Login) {
     const url = process.env.URL_API
 
     const response = await fetch(`${url}/login`, {
-        method: "post",
+        method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
